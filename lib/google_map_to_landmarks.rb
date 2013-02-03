775a2e1ad7a53e64ea9e8a57059a76caab12fd54
Alex Sorokin <alex@crowdflower.com>
2013-02-02 20:09:26 -0800

    cleanup

--- lib/google_map_to_landmarks.rb
+++ lib/google_map_to_landmarks.rb
@@ -12,13 +12,7 @@ default_map_url='http://maps.google.com/maps/ms?authuser=0&vps=3&hl=en&ie=UTF8&m
 
 map_url = ARGV[0] || default_map_url
 
-
-#p open(map_url).read
 doc = Nokogiri::XML( open(map_url) )
-
-#p doc
-#p doc.xpath('//Placemark').first.name
-
 
 paths = []
 doc.xpath('/kml/Document/Placemark').each do |path|
@@ -28,14 +22,5 @@ doc.xpath('/kml/Document/Placemark').each do |path|
   long_lat_path = path.strip.split("\n").map {|l| a = l.strip.split(',').map {|v| v.to_f}; [a[1],a[0]]}
   paths << {:name => landmark_name, :path =>long_lat_path, :type => 'path'}
 end
-
-#p paths
-
-# doc.xpath('//item[gml:Polygon]').each do |path|
-#   landmark_name = path.xpath("title").first.text
-#   path = path.xpath("gml:Polygon/gml:exterior/gml:LinearRing/gml:posList").first.text
-#   long_lat_path = path.strip.split("\n").map {|l| l.strip.split(' ').map {|v| v.to_f}}
-#   paths << {:name => landmark_name, :path =>long_lat_path, :type => 'path'}
-# end
 
 puts YAML.dump(paths)
